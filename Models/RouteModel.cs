1b8ff13b0ba8a0486df5b23949383cbe800500a6
KGP1998 <106868667+KGP1998@users.noreply.github.com>
2022-06-04 19:41:53 +0530

    model-view

---
+++ Models/RouteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewStackExam.Models
+{
+    public class RouteModel
+    {
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string ViewName { get; set; }
+        public string Url { get; set; }
+        public string RouteName { get; set; }
+    }
+}
